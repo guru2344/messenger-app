07c2e99a2f05c2691c43bf1eb18f4d07d6428d3f
xzguru72-blip <guruprasad003b@gmail.com>
2026-04-23 14:08:26 +0530

    railway db

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,35 @@
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root123",
+//   database: "messenger"
+// });
+
+// db.connect((err) => {
+//   if (err) console.log(err);
+//   else console.log("MySQL Connected");
+// });
+
+// module.exports = db;
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "messenger"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
-  if (err) console.log(err);
-  else console.log("MySQL Connected");
+  if (err) {
+    console.log("DB ERROR:", err);
+  } else {
+    console.log("✅ MySQL Connected");
+  }
 });
 
 module.exports = db;
